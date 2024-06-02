a460781c4a59094188300301d8ef759c2f099ede
MahmoudESSE <mahmoudessehayli@gmail.com>
2024-06-02 17:28:32 +0100

    feat: add vercel analytics
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { api } from "@/utils/api";
 
@@ -20,6 +21,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <main className={`font-sans ${inter.variable}`}>
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </SessionProvider>
   );
